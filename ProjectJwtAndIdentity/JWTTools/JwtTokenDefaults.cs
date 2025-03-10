0e0bdebead36e200d957808a29c4fddf9b0d17ce
Elif Yıldırım <elifyildirim3@outlook.com>
2025-03-10 23:54:01 +0300

    Ders 47 start

---
+++ ProjectJwtAndIdentity/JWTTools/JwtTokenDefaults.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectJwtAndIdentity.JWTTools;
+
+public class JwtTokenDefaults
+{
+    //const sabit anlamı taşır ,valid : doğrulama anlamında, Audience : dinleyiicim
+
+    public const string ValidAudience = "https://localhost"; //yani bu token kim tarafından dinlensin,bu dileyicim localhost tarafından dinlensin dedik.
+    public const string ValidIssuer = "https://localhost"; //bu da yayıncı yani tokenı oluşturan kim
+    public const string Key = ""; //bu token için bir tane key verelim, bu key bizim tokenı aslında kendi localimde kendine ait şifresi olacak ve bu keyin uzun olması gerekiyor. 
+     
+}
